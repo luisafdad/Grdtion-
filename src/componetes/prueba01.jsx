41ec42640667135afa70223a9fecb3e7bcfa64c6
wcastro <wcastro@cotecmar.com>
2021-11-27 23:42:41 -0500

    Prueba de Concepto Bootstrap

--- src/componetes/prueba01.jsx
+++ src/componetes/prueba01.jsx
@@ -3,14 +3,15 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 export const Contar = () => {
 
-    const [numeros, setNumeros] = usState(0);
+    const [numeros, setNumeros] = useState(0);
 
-    return
-        <div className='ner-fluid m-3 p-4'>
+    return(
+        <div className='conteiner-fluid m-3 p-4'>
             <p>{numeros}</p>
-            <button>+</button>
-            <button>-</button>
+            <button className='btn btn-primary btn-lg'>+</button>
+            <button className='btn btn-secondary btn-lg'>-</button>
 
         </div>
+    );
 
 }
