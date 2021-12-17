e0ebe80127d0a98f902908e45bf7c45ba1303d26
Leidy Lorena Valderrama <lorenavalderrama@usc.edu.co>
2021-12-17 13:36:38 -0500

    Crud predio mocks

--- src/componentes/paginas/PredialConsul.js
+++ src/componentes/paginas/PredialConsul.js
@@ -1,26 +1,15 @@
 import React from "react";
 import NavbarExtUser from "../navegacion/NavbarExtUser";
 
-import { useState, useEffect } from "react";
-import PrediosTable from "../tables/PrediosTable"
+import PrediosTable from "../tables/PrediosTable";
 
-const PredialConsul = () => {
-
-    const [ prediosApi, setPrediosApi] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:3004/predios')
-        .then(response => response.json())
-        .then(data => setPrediosApi(data));
-    }, []);
-
+const PredialConsul = (props) => {
 
     return (
         <div>
             <NavbarExtUser />
             <section className="page-section bg-white">
                 <div className="container">
-
                     <div className="card shadow mb-4">
                         <div className="card-header py-3">
                             <h6 className="m-0 font-weight-bold text-primary">
@@ -29,7 +18,8 @@ const PredialConsul = () => {
                         </div>
                         <div className="card-body">
                             <div className="table-responsive">
-                                <PrediosTable predios={prediosApi} />
+                                <PrediosTable onDelete={props.onDelete}
+                                    predios={props.predios} />
                             </div>
                         </div>
                     </div>
